b20c6899ef2ec0be58d1b6157bdaa12cd920364d
sohailsabir <sohailsabir6565@gmail.com>
2021-08-26 16:39:42 +1200

    App bar with title

---
+++ appbarwithtitle/android/app/src/main/kotlin/com/example/appbar_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appbar_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ appbarwithtitle/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Home",),
+        elevation: 10.0,
+      ),
+    ),
+  ));
+}
