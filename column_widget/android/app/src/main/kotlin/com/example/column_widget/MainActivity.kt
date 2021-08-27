ad33b79a21cd9a53d1ee34b0734ea70306165e85
sohailsabir <sohailsabir6565@gmail.com>
2021-08-27 17:21:11 +1200

    container

---
+++ column_widget/android/app/src/main/kotlin/com/example/column_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.column_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ column_widget/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Text("Column 1", ),
+            Text("Column 2", ),
+            Text("Column 3", )
+          ],
+        )
+        ,
+      ),
+    );
+  }
+}
