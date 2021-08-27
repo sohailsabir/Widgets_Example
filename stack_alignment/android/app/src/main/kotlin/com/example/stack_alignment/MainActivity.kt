99c8822e5546028d3b8a4e42e18da727c35cde0d
sohailsabir <sohailsabir6565@gmail.com>
2021-08-27 17:02:40 +1200

    stack with alignment propertise

---
+++ stack_alignment/android/app/src/main/kotlin/com/example/stack_alignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stack_alignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ stack_alignment/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              height: 300.0,
+              width: 300.0,
+              color: Colors.red,
+            ),
+            Container(
+              height: 250.0,
+              width: 250.0,
+              color: Colors.green,
+            ),
+            Container(
+              height: 200.0,
+              width: 200.0,
+              color: Colors.yellow,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
