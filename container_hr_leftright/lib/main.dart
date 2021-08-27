7800b29da98f7aa22833627f65f50a25bac55abe
sohailsabir <sohailsabir6565@gmail.com>
2021-08-27 17:19:39 +1200

    container with horizotal radius left and right

---
+++ container_hr_leftright/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body:  Center(
+          child: Container(
+            height: 200.0,
+            width: 200.0,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.horizontal(
+                  left: Radius.circular(20.0),
+                  right: Radius.circular(80.0)
+              ),
+              color: Colors.green,
+            ),
+            child: Text('Hello'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
